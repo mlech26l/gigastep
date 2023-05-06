c328f40cb127db6bd1d8d4a09d867860bed51377
Mathias Lechner <mlech26l@gmail.com>
2023-05-06 16:22:26 +0200

    no optax, flax in jax_utils.py

--- gigastep/jax_utils.py
+++ gigastep/jax_utils.py
@@ -1,7 +1,8 @@
 import jax
-import optax
 import jax.numpy as jnp
-from flax.training import train_state
+
+# import optax
+# from flax.training import train_state
 
 
 class Box:
@@ -43,12 +44,12 @@ class Discrete:
         return jax.random.randint(rng, (), 0, self.n)
 
 
-def create_train_state(model, rng, in_dim, learning_rate, ema=0, clip_norm=None):
-    """Creates initial `TrainState`."""
-    params = model.init(rng, jnp.ones([1, in_dim]))
-    tx = optax.adam(learning_rate)
-    if clip_norm is not None:
-        tx = optax.chain(tx, optax.clip_by_global_norm(clip_norm))
-    if ema > 0:
-        tx = optax.chain(tx, optax.ema(ema))
-    return train_state.TrainState.create(apply_fn=model.apply, params=params, tx=tx)
+# def create_train_state(model, rng, in_dim, learning_rate, ema=0, clip_norm=None):
+#     """Creates initial `TrainState`."""
+#     params = model.init(rng, jnp.ones([1, in_dim]))
+#     tx = optax.adam(learning_rate)
+#     if clip_norm is not None:
+#         tx = optax.chain(tx, optax.clip_by_global_norm(clip_norm))
+#     if ema > 0:
+#         tx = optax.chain(tx, optax.ema(ema))
+#     return train_state.TrainState.create(apply_fn=model.apply, params=params, tx=tx)

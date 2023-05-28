cfa9ef61edcd84b5546e0d31f55675eb15012d3e
Mathias Lechner <mlech26l@gmail.com>
2023-05-27 20:17:19 -0400

    evaluator batched and unbatched

--- examples/test_evaluator.py
+++ examples/test_evaluator.py
@@ -4,6 +4,7 @@ import time
 
 import cv2
 import jax
+import numpy as np
 
 from gigastep import GigastepViewer, GigastepEnv, make_scenario, ScenarioBuilder
 from gigastep.evaluator import Evaluator
@@ -50,8 +51,45 @@ def loop_env(env):
             #     sys.exit(1)
 
 
+def loop_env_vectorized(env):
+    evaluator = Evaluator(env)
+    batch_size = 20
+    rng = jax.random.PRNGKey(3)
+
+    for opponent in evaluator.policies:
+        print("Opponent", str(opponent))
+        for ep_idx in range(5):
+            ep_done = np.zeros(batch_size, dtype=jnp.bool_)
+            key, rng = jax.random.split(rng, 2)
+            key = jax.random.split(key, batch_size)
+            state, obs = env.v_reset(key)
+            while not jnp.all(ep_done):
+                rng, key, key2 = jax.random.split(rng, 3)
+                action_ego = jnp.zeros(
+                    (batch_size, env.n_agents, 3)
+                )  # ego does nothing
+                key2 = jax.random.split(key2, batch_size)
+                action_opp = opponent.v_apply(obs, key2)
+
+                action = evaluator.v_merge_actions(action_ego, action_opp)
+
+                rng, key = jax.random.split(rng, 2)
+                key = jax.random.split(key, batch_size)
+                state, obs, r, dones, ep_done = env.v_step(state, action, key)
+                evaluator.update_step(r, dones, ep_done)
+
+                time.sleep(SLEEP_TIME)
+            evaluator.update_episode()
+            print(str(evaluator))
+            # if frame_idx > 400:
+            #     sys.exit(1)
+
+
 if __name__ == "__main__":
     # convert -delay 3 -loop 0 video/scenario/frame_*.png video/scenario.webp
+    loop_env_vectorized(
+        env=make_scenario("identical_20_vs_20", use_stochastic_comm=False)
+    )
     loop_env(env=make_scenario("identical_20_vs_20", use_stochastic_comm=False))
     # loop_env(env = make_scenario("identical_20_vs_20"))
     # loop_env(env = make_scenario("identical_20_vs_20"))

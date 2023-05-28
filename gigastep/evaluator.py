1f039fc80a2956ca516f99e32fbf6c59b7a03833
lianhaoyin <lianhao.yin@gmail.com>
2023-05-28 16:59:27 -0400

    Add troch_policy class in evluator class

--- gigastep/evaluator.py
+++ gigastep/evaluator.py
@@ -8,16 +8,60 @@ import numpy as np
 
 from gigastep import GigastepViewer 
 
+SLEEP_TIME = 0.01
+
 
 class EvaluatiorPolicy:
     def apply(self, obs, rng):
         raise NotImplementedError()
 
-class Trained_policy(EvaluatiorPolicy):
-    def __init__(self, env, policy):
-        pass
+class Torch_Policy(EvaluatiorPolicy):
+    def __init__(self, env, policy, device = "cpu", vectorize = False):
+        self.actor_critic = policy
+        if vectorize:
+            self.recurrent_hidden_states = torch.zeros(env.n_agents,
+                          policy.recurrent_hidden_state_size,
+                          device=device
+            ) if policy is not None else None
+        else:
+            self.recurrent_hidden_states = torch.zeros(env.n_agents,
+                              policy.recurrent_hidden_state_size,
+                              device=device
+                ) if policy is not None else None
+        self.n_ego_agents = env.n_teams[0]
+        self.device = device
+        self.vectorize = vectorize
+        self.discrete_actions = env.discrete_actions
+
     def apply(self, obs, rng):
-        raise NotImplementedError()
+
+        obs = torch.tensor(np.asarray(obs), device=self.device)
+
+        if not self.vectorize:
+            obs = torch.unsqueeze(obs, 0)
+        obs = torch.moveaxis(obs, -1, 2)
+        n_opp = obs.shape[1] - self.n_ego_agents
+        obs = obs[:, :self.n_ego_agents, ::]
+        obs = obs.float().contiguous()
+        masks = torch.ones(1, 1).to(self.device)
+        with torch.no_grad():
+            value, action, _, self.recurrent_hidden_states = self.actor_critic.act(
+                obs.float(), self.recurrent_hidden_states, masks, deterministic=True)
+        if not self.vectorize:
+            action = torch.squeeze(action, 0)
+
+        action = action.squeeze(-1)
+
+        if self.discrete_actions:
+            action = jnp.array(action.detach().cpu().numpy().astype(np.int32))
+        else:
+            action = jnp.array(action.detach().cpu().numpy().astype(np.float32))
+        if self.vectorize:
+            action = jnp.pad(action, ((0, 0),(0,n_opp)), 'constant', constant_values=(0))
+        else:
+            action = jnp.pad(action, ((0, n_opp)), 'constant', constant_values=(0))
+
+        return action
 
 class RandomPolicy(EvaluatiorPolicy):
     def __init__(self, env):
@@ -170,3 +214,105 @@ class Evaluator:
         self._ep_rewards.append(r)
         self._ep_alives.append(a)
         self._ep_dones.append(ep_done)
+
+
+
+
+def loop_env(env, policy=None, device="cpu", headless=False):
+    evaluator = Evaluator(env)
+    viewer = GigastepViewer(frame_size=84 * 2, show_num_agents=0 if env.discrete_actions else env.n_agents)
+
+    rng = jax.random.PRNGKey(3)
+
+    for opponent in evaluator.policies:
+        print("Opponent", str(opponent))
+        for ep_idx in range(5):
+            ep_done = False
+            key, rng = jax.random.split(rng, 2)
+            state, obs = env.reset(key)
+            
+            ego = Torch_Policy(policy=policy,env=env,device=device,vectorize = False)
+            
+            while not ep_done:
+                rng, key, key2 = jax.random.split(rng, 3)
+                if policy is None:
+                    action_ego = jnp.zeros(
+                        (env.n_agents, 3)
+                    )  # ego does nothing
+                else:
+                    action_ego = ego.apply(obs, key2)
+
+                action_opp = opponent.apply(obs, key2)
+
+                action = evaluator.merge_actions(action_ego, action_opp)
+
+                rng, key = jax.random.split(rng, 2)
+                state, obs, r, dones, ep_done = env.step(state, action, key)
+                evaluator.update_step(r, dones, ep_done)
+                if not headless:
+                    img = viewer.draw(env, state, obs)
+                    # if viewer.should_pause:
+                    #     while True:
+                    #         img = viewer.draw(env, state, obs)
+                    #         time.sleep(SLEEP_TIME)
+                    #         if viewer.should_pause:
+                    #             break
+                    if viewer.should_quit:
+                        sys.exit(1)
+                time.sleep(SLEEP_TIME)
+            evaluator.update_episode()
+            print(str(evaluator))
+            # if frame_idx > 400:
+            #     sys.exit(1)
+        break
+    return [evaluator.team_a_wins / evaluator.total_games,
+            evaluator.team_b_wins / evaluator.total_games]
+
+
+def loop_env_vectorized(env, policy=None, device="cpu"):
+    evaluator = Evaluator(env)
+    batch_size = 20
+    rng = jax.random.PRNGKey(3)
+
+    for opponent in evaluator.policies:
+        print("Opponent", str(opponent))
+        for ep_idx in range(1):
+            ep_done = np.zeros(batch_size, dtype=jnp.bool_)
+            key, rng = jax.random.split(rng, 2)
+            key = jax.random.split(key, batch_size)
+            state, obs = env.v_reset(key)
+            t = 0
+
+            ego = Torch_Policy(policy=policy,env=env,device=device,vectorize = True)
+            
+            while not jnp.all(ep_done):
+                rng, key, key2 = jax.random.split(rng, 3)
+                if policy is None:
+                    action_ego = jnp.zeros(
+                        (batch_size, env.n_agents, 3)
+                    )  # ego does nothing
+                else:
+                    action_ego = ego.apply(obs, key2)
+                
+                key2 = jax.random.split(key2, batch_size)
+                action_opp = opponent.v_apply(obs, key2)
+
+                action = evaluator.v_merge_actions(action_ego, action_opp)
+
+                rng, key = jax.random.split(rng, 2)
+                key = jax.random.split(key, batch_size)
+                state, obs, r, dones, ep_done = env.v_step(state, action, key)
+                evaluator.update_step(r, dones, ep_done)
+
+                time.sleep(SLEEP_TIME)
+                t += 1
+                # print("t", t, "ep_done", ep_done)
+            evaluator.update_episode()
+            print(str(evaluator))
+            # if frame_idx > 400:
+            #     sys.exit(1)
+        break
+
+    return [evaluator.team_a_wins / evaluator.total_games,
+            evaluator.team_b_wins / evaluator.total_games]
+

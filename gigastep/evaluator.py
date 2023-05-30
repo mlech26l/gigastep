36a726724f10296b0997257925ba988cf2a9802c
Mathias Lechner <mlech26l@gmail.com>
2023-05-30 17:29:26 -0400

    Human vs circling agent

--- gigastep/evaluator.py
+++ gigastep/evaluator.py
@@ -6,7 +6,7 @@ import time
 
 import numpy as np
 
-from gigastep import GigastepViewer 
+from gigastep import GigastepViewer
 
 SLEEP_TIME = 0.01
 
@@ -15,38 +15,45 @@ class EvaluatiorPolicy:
     def apply(self, obs, rng):
         raise NotImplementedError()
 
+
 class Torch_Policy(EvaluatiorPolicy):
-    def __init__(self, env, policy, device = "cpu", vectorize = False):
+    def __init__(self, env, policy, device="cpu", vectorize=False):
         self.actor_critic = policy
         if vectorize:
-            self.recurrent_hidden_states = torch.zeros(env.n_agents,
-                          policy.recurrent_hidden_state_size,
-                          device=device
-            ) if policy is not None else None
+            self.recurrent_hidden_states = (
+                torch.zeros(
+                    env.n_agents, policy.recurrent_hidden_state_size, device=device
+                )
+                if policy is not None
+                else None
+            )
         else:
-            self.recurrent_hidden_states = torch.zeros(env.n_agents,
-                              policy.recurrent_hidden_state_size,
-                              device=device
-                ) if policy is not None else None
+            self.recurrent_hidden_states = (
+                torch.zeros(
+                    env.n_agents, policy.recurrent_hidden_state_size, device=device
+                )
+                if policy is not None
+                else None
+            )
         self.n_ego_agents = env.n_teams[0]
         self.device = device
         self.vectorize = vectorize
         self.discrete_actions = env.discrete_actions
 
     def apply(self, obs, rng):
-
         obs = torch.tensor(np.asarray(obs), device=self.device)
 
         if not self.vectorize:
             obs = torch.unsqueeze(obs, 0)
         obs = torch.moveaxis(obs, -1, 2)
         n_opp = obs.shape[1] - self.n_ego_agents
-        obs = obs[:, :self.n_ego_agents, ::]
+        obs = obs[:, : self.n_ego_agents, ::]
         obs = obs.float().contiguous()
         masks = torch.ones(1, 1).to(self.device)
         with torch.no_grad():
             value, action, _, self.recurrent_hidden_states = self.actor_critic.act(
-                obs.float(), self.recurrent_hidden_states, masks, deterministic=True)
+                obs.float(), self.recurrent_hidden_states, masks, deterministic=True
+            )
         if not self.vectorize:
             action = torch.squeeze(action, 0)
 
@@ -57,11 +64,14 @@ class Torch_Policy(EvaluatiorPolicy):
         else:
             action = jnp.array(action.detach().cpu().numpy().astype(np.float32))
         if self.vectorize:
-            action = jnp.pad(action, ((0, 0),(0,n_opp)), 'constant', constant_values=(0))
+            action = jnp.pad(
+                action, ((0, 0), (0, n_opp)), "constant", constant_values=(0)
+            )
         else:
-            action = jnp.pad(action, ((0, n_opp)), 'constant', constant_values=(0))
+            action = jnp.pad(action, ((0, n_opp)), "constant", constant_values=(0))
 
         return action
+
 
 class RandomPolicy(EvaluatiorPolicy):
     def __init__(self, env):
@@ -114,20 +124,31 @@ class CircleRandomPolicy(EvaluatiorPolicy):
     def __init__(self, env):
         self.env = env
         if self.env.discrete_actions:
-            raise NotImplementedError()
+            self.action1 = jnp.argmin(
+                jnp.linalg.norm(self.env.action_lut - jnp.array([[1, 0, 0]]), axis=1)
+            )
+            self.action2 = jnp.argmin(
+                jnp.linalg.norm(self.env.action_lut - jnp.array([[-1, 0, 0]]), axis=1)
+            )
         self.v_apply = jax.vmap(self.apply, in_axes=(0, 0))
 
     def apply(self, obs, rng):
         n_agents = obs.shape[0]
-        directions = (
-            jax.random.randint(
+        if self.env.discrete_actions:
+            directions = jax.random.randint(
                 jax.random.PRNGKey(1), shape=(n_agents,), minval=0, maxval=2
             )
-            - 1
-        )
-        actions = jnp.stack(
-            [directions, jnp.zeros(n_agents), jnp.zeros(n_agents)], axis=1
-        )
+            actions = jnp.where(directions == 1, self.action1, self.action2)
+        else:
+            directions = (
+                jax.random.randint(
+                    jax.random.PRNGKey(1), shape=(n_agents,), minval=0, maxval=2
+                )
+                - 1
+            )
+            actions = jnp.stack(
+                [directions, jnp.zeros(n_agents), jnp.zeros(n_agents)], axis=1
+            )
         return actions
 
     def __str__(self):
@@ -141,7 +162,7 @@ class Evaluator:
             RandomPolicy(env),
             CirclePolicy(env),
             CirclePolicy(env, direction=-1),
-            # CircleRandomPolicy(env),
+            CircleRandomPolicy(env),
         ]
         self._ep_rewards = []
         self._ep_alives = []
@@ -165,7 +186,11 @@ class Evaluator:
 
     @property
     def tie_rate(self):
-        return (self.total_games - self.team_a_wins - self.team_b_wins) * 100 / self.total_games
+        return (
+            (self.total_games - self.team_a_wins - self.team_b_wins)
+            * 100
+            / self.total_games
+        )
 
     def __str__(self):
         return (
@@ -194,7 +219,6 @@ class Evaluator:
 
         team_a_rewards = jnp.where(self.env.teams[None, :] == 0, alive_rewards, 0)
         team_b_rewards = jnp.where(self.env.teams[None, :] == 1, alive_rewards, 0)
-
 
         self.team_a_reward += team_a_rewards.sum()
         self.team_b_reward += team_b_rewards.sum()
@@ -241,11 +265,11 @@ class Evaluator:
         self._ep_dones.append(ep_done)
 
 
-
-
 def loop_env(env, policy=None, device="cpu", headless=False):
     evaluator = Evaluator(env)
-    viewer = GigastepViewer(frame_size=84 * 2, show_num_agents=0 if env.discrete_actions else env.n_agents)
+    viewer = GigastepViewer(
+        frame_size=84 * 2, show_num_agents=0 if env.discrete_actions else env.n_agents
+    )
 
     rng = jax.random.PRNGKey(3)
 
@@ -255,15 +279,13 @@ def loop_env(env, policy=None, device="cpu", headless=False):
             ep_done = False
             key, rng = jax.random.split(rng, 2)
             state, obs = env.reset(key)
-            
-            ego = Torch_Policy(policy=policy,env=env,device=device,vectorize = False)
-            
+
+            ego = Torch_Policy(policy=policy, env=env, device=device, vectorize=False)
+
             while not ep_done:
                 rng, key, key2 = jax.random.split(rng, 3)
                 if policy is None:
-                    action_ego = jnp.zeros(
-                        (env.n_agents, 3)
-                    )  # ego does nothing
+                    action_ego = jnp.zeros((env.n_agents, 3))  # ego does nothing
                 else:
                     action_ego = ego.apply(obs, key2)
 
@@ -290,8 +312,10 @@ def loop_env(env, policy=None, device="cpu", headless=False):
             # if frame_idx > 400:
             #     sys.exit(1)
         break
-    return [evaluator.team_a_wins / evaluator.total_games,
-            evaluator.team_b_wins / evaluator.total_games]
+    return [
+        evaluator.team_a_wins / evaluator.total_games,
+        evaluator.team_b_wins / evaluator.total_games,
+    ]
 
 
 def loop_env_vectorized(env, policy=None, device="cpu"):
@@ -308,8 +332,8 @@ def loop_env_vectorized(env, policy=None, device="cpu"):
             state, obs = env.v_reset(key)
             t = 0
 
-            ego = Torch_Policy(policy=policy,env=env,device=device,vectorize = True)
-            
+            ego = Torch_Policy(policy=policy, env=env, device=device, vectorize=True)
+
             while not jnp.all(ep_done):
                 rng, key, key2 = jax.random.split(rng, 3)
                 if policy is None:
@@ -318,7 +342,7 @@ def loop_env_vectorized(env, policy=None, device="cpu"):
                     )  # ego does nothing
                 else:
                     action_ego = ego.apply(obs, key2)
-                
+
                 key2 = jax.random.split(key2, batch_size)
                 action_opp = opponent.v_apply(obs, key2)
 
@@ -338,6 +362,7 @@ def loop_env_vectorized(env, policy=None, device="cpu"):
             #     sys.exit(1)
         break
 
-    return [evaluator.team_a_wins / evaluator.total_games,
-            evaluator.team_b_wins / evaluator.total_games]
-
+    return [
+        evaluator.team_a_wins / evaluator.total_games,
+        evaluator.team_b_wins / evaluator.total_games,
+    ]
